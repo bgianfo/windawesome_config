ccc0a45c03c5b651ab707b49420d1486779d32ff
Brian Gianforcaro <b.gianfo@gmail.com>
2014-04-18 13:08:29 -0700

    Add new rules for laying out programs in workspaces.

--- 2-Program Rules.py
+++ 2-Program Rules.py
@@ -2,51 +2,78 @@ from Windawesome import Windawesome, ProgramRule, State, OnWindowCreatedOrShownA
 from Windawesome.NativeMethods import WS, WS_EX
 
 config.ProgramRules = [
-	ProgramRule(
-		className = "^cygwin/x X rl$",
-		windowCreatedDelay = 300,
-		rules = [ProgramRule.Rule(workspace = 1)]
-	),
+
 	ProgramRule(
 		className = "^TApplication$",
 		rules = [ProgramRule.Rule(isFloating = True)]
 	),
+
+	# Browsers
+
+    # Chrome
+	ProgramRule(
+		className = "^Chrome_WidgetWin_1$",
+		rules = [ProgramRule.Rule(workspace = 2, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
+	),
+    # Internet Explorer
+	ProgramRule(
+		className = "^IEFrame$",
+		rules = [ProgramRule.Rule(workspace = 2, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
+	),
+	# Firefox
 	ProgramRule(
 		className = "^MozillaWindowClass$",
 		rules = [ProgramRule.Rule(workspace = 2)]
 	),
+	# Firefox Modal
 	ProgramRule(
 		className = "^MozillaDialogClass$",
 		rules = [ProgramRule.Rule(workspace = 2, isFloating = True)]
 	),
+
+	# Windows Explorer
 	ProgramRule(
-		className = "^CabinetWClass$", # Windows Explorer
+		className = "^CabinetWClass$",
 		updateIcon = True,
-		rules = [ProgramRule.Rule(workspace = 1)]
+		rules = [ProgramRule.Rule(workspace = 1, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 	ProgramRule(
-		className = "^ExploreWClass$", # Windows Explorer
+		className = "^ExploreWClass$",
 		updateIcon = True,
-		rules = [ProgramRule.Rule(workspace = 1)]
-	),
-	ProgramRule(
-		displayName = ".*SA Dictionary 2010.*",
-		rules = [ProgramRule.Rule(isFloating = True)]
-	),
-	ProgramRule(
-		className = "^rctrl_renwnd32$", # Outlook
-		rules = [ProgramRule.Rule(workspace = 5)]
-	),
-	ProgramRule(
-		className = "^{97E27FAA-C0B3-4b8e-A693-ED7881E99FC1}$", # foobar2000
-		rules = [ProgramRule.Rule(workspace = 1)]
+		rules = [ProgramRule.Rule(workspace = 1, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 
-	# editors
+	# Outlook
+	ProgramRule(
+		className = "^rctrl_renwnd32$",
+		rules = [ProgramRule.Rule(workspace = 5, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
+	),
 
+    # Lync
+	ProgramRule(
+		className = "^CommunicatorMainWindowClass$",
+		rules = [ProgramRule.Rule(workspace = 4, isFloating = True)]
+	),
+	ProgramRule(
+		className = "^IMWindowClass$",
+		redrawDesktopOnWindowCreated = True,
+		onWindowCreatedAction = OnWindowCreatedOrShownAction.HideWindow,
+		onWindowCreatedOnCurrentWorkspaceAction = OnWindowCreatedOnWorkspaceAction.PreserveTopmostWindow,
+		onWindowCreatedOnInactiveWorkspaceAction = OnWindowCreatedOnWorkspaceAction.PreserveTopmostWindow,
+		rules = [ProgramRule.Rule(workspace = 4, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
+	),
+
+
+	# Spotify Music
+	ProgramRule(
+		className="^SpotifyMainWindow$",
+		rules = [ProgramRule.Rule(workspace = 1)]
+	),
+	
+	# Editors
 	ProgramRule(
 		className = "^Vim$",
-		#windowCreatedDelay = 100,
+		windowCreatedDelay = 100,
 		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 	ProgramRule(
@@ -54,26 +81,17 @@ config.ProgramRules = [
 		onWindowCreatedAction = OnWindowCreatedOrShownAction.HideWindow,
 		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
-
-	# chat
-
 	ProgramRule(
-		className = "^icoTrilly$",
-		onWindowCreatedAction = OnWindowCreatedOrShownAction.TemporarilyShowWindowOnCurrentWorkspace,
-		onWindowCreatedOnInactiveWorkspaceAction = OnWindowCreatedOnWorkspaceAction.PreserveTopmostWindow,
-		rules = [ProgramRule.Rule(workspace = 4, isFloating = True)]
+		processName = "^devenv$",
+		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 	ProgramRule(
-		className = "^ico.*",
-		processName = "^trillian$",
+		displayName = ".*Microsoft SQL Server Management Studio.*",
 		onWindowCreatedAction = OnWindowCreatedOrShownAction.HideWindow,
-		onWindowCreatedOnCurrentWorkspaceAction = OnWindowCreatedOnWorkspaceAction.PreserveTopmostWindow,
-		onWindowCreatedOnInactiveWorkspaceAction = OnWindowCreatedOnWorkspaceAction.PreserveTopmostWindow,
-		rules = [ProgramRule.Rule(workspace = 4)]
+		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 
-	# terminals
-
+	# Terminals
 	ProgramRule(
 		className = "^Console_2_Main$",
 		rules = [ProgramRule.Rule(workspace = 3)]
@@ -81,6 +99,15 @@ config.ProgramRules = [
 	ProgramRule(
 		className = "^mintty$",
 		redrawDesktopOnWindowCreated = True,
+		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
+	),
+	ProgramRule(
+		className = "^cygwin/x X rl$",
+		windowCreatedDelay = 300,
+		rules = [ProgramRule.Rule(workspace = 3)]
+	),
+	ProgramRule( # Console, Git Bash, Powershell
+		className = "^ConsoleWindowClass$",
 		rules = [ProgramRule.Rule(workspace = 3, titlebar = State.HIDDEN, windowBorders = State.HIDDEN)]
 	),
 
